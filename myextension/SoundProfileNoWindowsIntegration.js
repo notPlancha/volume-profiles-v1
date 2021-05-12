73adfd6ab09264f32ae957fc57c4daeb5cae51c7
notPlancha <andre.plancha@hotmail.com>
2021-05-12 20:33:12 +0100

    added what I want to do and check if it's working

--- myextension/SoundProfileNoWindowsIntegration.js
+++ myextension/SoundProfileNoWindowsIntegration.js
@@ -2,7 +2,7 @@
 
 // NAME: Sound Profiles
 // AUTHOR: notPlancha
-// DESCRIPTION: Create sound profiles that can adapt to the windows system sound (optional?)
+// DESCRIPTION: Create sound profiles
 
 /// <reference path="../globals.d.ts" />
 
@@ -11,14 +11,21 @@
     setTimeout(SoundProfiles, 1000);
     return;
   }
+
   class SoundProfile{
-    constructor(SpotifySoundValue, WindowsSoundValue, name){
+    //something something Spicetify.Player.getVolume
+    constructor(SpotifySoundValue, name){
       //TODO check what these are so i can think of a better
       this.name = name;
+      this.soundValue = SpotifySoundValue;
     }
     get(name){
       //TODO get from LocalStorage
     }
   }
+  Spicetify.showNotification("It's alive");
+  //new Spicetify.Menu
   //TODO if no SoundProfile create default SoundProfiles and apply
+
+  //TODO apply style to document.getElementsByClassName('extra-controls-container')[0] padding-top:30px; para ter espaco para por os controles em cima
 })();
